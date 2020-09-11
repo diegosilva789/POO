16694ecb0d3ee58a09a95ae667eb8253448360f3
diegosilva89 <56441292+diegosilva89@users.noreply.github.com>
2020-09-11 19:26:41 -0300

    POO-2020-08-25

---
+++ POO-2020-08-25/src/org/fatec/App.java
@@ -0,0 +1,14 @@
+package org.fatec;
+
+import javax.swing.JOptionPane;
+
+public class App {
+	// corpo da classe ou escopo
+	// aqui que os codigos executam
+	public static void main (String[] args) {
+		JOptionPane.showMessageDialog(null, "Texto Importante");
+		int psg = 1;
+		System.out.println(psg);
+		System.out.println("Texto Importante!!");
+	}
+}
